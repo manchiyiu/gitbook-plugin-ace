e26d1ddf3a98e8950ebe53c3ef392c272572e565
Yiu Man Chi <ymcatar@gmail.com>
2016-08-09 23:09:42 +0800

    remove: old code

--- index.js
+++ index.js
@@ -38,33 +38,6 @@ module.exports = {
 		]
 	},
 
-	// blocks: {
-	// 	ace: {
-	// 		process: function(blk) {
-	// 			if (this.generator === 'website') {
-	// 				var config = {
-	// 					edit: blk.kwargs.edit,
-	// 					lang: blk.kwargs.lang || 'c_cpp',
-	// 					check: blk.kwargs.check,
-	// 					theme: blk.kwargs.theme
-	// 				};
-	// 				return '<div class="ace"><div class="aceCode" data-config=' + JSON.stringify(config) + '>' + escape(blk.body.trim()) + '<br></div></div>';
-	// 			} else {
-	// 				var content;
-	// 				var lang = blk.kwargs.lang || 'c_cpp';
-	// 				lang = map[lang] || lang;
-
-	// 				if (hljs.getLanguage(lang))
-	// 					content = hljs.highlight(lang, blk.body.trim()).value;
-	// 				else
-	// 					content = blk.body.trim();
-
-	// 				return '<pre><code class="hljs lang-' + lang + '">' + content + '</code></pre>';
-	// 			}
-	// 		}
-	// 	}
-	// },
-
 	hooks: {
 		'page:before': function (page) {
 			var self = this;
